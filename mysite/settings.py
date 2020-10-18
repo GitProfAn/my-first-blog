c1357d3937c218baa0b7098b58ffc200c9118be3
GitProfAn <serzhe@mail.ru>
2020-10-18 19:10:35 +0300

    Unused import removed

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-from .settings_secret import *
+# from .settings_secret import *
 from pathlib import Path
 import os
 
